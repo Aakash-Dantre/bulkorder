1c9618757c966d25d5e060363e5a63dcc2321917
Aakash-Dantre <aka.dantre@gmail.com>
2020-10-08 07:28:12 +0530

    Updates

---
+++ precache-manifest.7ef925aac40b067f2ff06d97569904a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea0169da361a62628912672b126f855",
+    "url": "/bulkorder/index.html"
+  },
+  {
+    "revision": "d4ad33425482638a6894",
+    "url": "/bulkorder/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d636925fea4ed890da78",
+    "url": "/bulkorder/static/css/main.2fb7ba53.chunk.css"
+  },
+  {
+    "revision": "d4ad33425482638a6894",
+    "url": "/bulkorder/static/js/2.9cbe4ee7.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/bulkorder/static/js/2.9cbe4ee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d636925fea4ed890da78",
+    "url": "/bulkorder/static/js/main.b5c9efd0.chunk.js"
+  },
+  {
+    "revision": "6849c7b6268fe37bd988",
+    "url": "/bulkorder/static/js/runtime-main.4ca18952.js"
+  }
+]);
